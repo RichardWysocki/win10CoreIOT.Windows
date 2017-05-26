f84c88c75b0cefabf91b654f07848d9f8abd4cbe
Richard Wysocki <richardwysocki@gmail.com>
2017-05-26 15:29:00 -0400

    Love my DB Project

---
+++ win10Core.Database/dbo/Stored Procedures/Customer_List.sql
@@ -0,0 +1,18 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE Customer_List
+	-- Add the parameters for the stored procedure here
+
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	SELECT CustomerID, FirstName, LastName from Customer
+
+END
---
+++ win10Core.Database/dbo/Tables/Customer.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Customer] (
+    [CustomerID] INT          IDENTITY (1, 1) NOT NULL,
+    [FirstName]  VARCHAR (50) NULL,
+    [LastName]   VARCHAR (50) NULL
+);
+
