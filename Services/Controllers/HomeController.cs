b7b25665f5ab0dc7d81ab80d58f50dc0b7b1dc7e
Richard Wysocki <richardwysocki@gmail.com>
2017-06-21 22:37:42 -0400

    Update Code

--- Services/Controllers/HomeController.cs
+++ Services/Controllers/HomeController.cs
@@ -26,32 +26,7 @@ namespace Services.Controllers
 
             var response = _serviceLayer.GetData<LogInfo>();
 
-
-
-
             return View();
         }
-
-        //private static List<LogInfo> GetData()
-        //{
-        //    var getData = new HttpClient();
-        //    var response = getData.GetAsync(new Uri("http://localhost:34909/api/LogInfo")).Result;
-        //    var data = response.Content.ReadAsAsync<List<LogInfo>>().Result;
-
-        //    return data;
-        //}
-
-
-//        private static void SendData()
-//        {
-//            var getData = new HttpClient();
-//            var objectData = new LogInfo { Method = "SendData", Message = "MyFirstMessage"};
-//            var response = getData.PostAsJsonAsync(new Uri("http://localhost:34909/api/LogInfo"), objectData).Result;
-//            if (!response.IsSuccessStatusCode) 
-//                throw new Exception("This didn't work!!!");
-////            var data = response.Content.ReadAsAsync<List<LogInfo>>().Result;
-
-// //           return data;
-//        }
     };
 }
