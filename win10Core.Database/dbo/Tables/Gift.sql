691a62abc14035fb161ff972971152e426fcf457
Richard Wysocki <richardwysocki@gmail.com>
2017-09-27 22:57:38 -0400

    Add updated DataBase Changes

---
+++ win10Core.Database/dbo/Tables/Gift.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[Gift] (
+    [GiftID]     INT           IDENTITY (1, 1) NOT NULL,
+    [KidID]      INT           NULL,
+    [GiftName]   VARCHAR (200) NULL,
+    [Priority]   INT           NULL,
+    [WebUrl]     VARCHAR (200) NULL,
+    [CreateDate] DATETIME      CONSTRAINT [DF_Gift_CreateDate] DEFAULT (getdate()) NULL,
+    [EmailSent]  BIT           CONSTRAINT [DF_Gift_EmailSent] DEFAULT ((0)) NULL,
+    CONSTRAINT [PK_Gift] PRIMARY KEY CLUSTERED ([GiftID] ASC)
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [NonClusteredIndex-20170915-145813]
+    ON [dbo].[Gift]([EmailSent] ASC);
+
