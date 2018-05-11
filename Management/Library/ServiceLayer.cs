6211cda4f63ac8a1f11155838ff370b907d4562b
Richad Wysocki <RichardWysocki@gmail.com>
2018-05-10 21:51:47 -0400

    Add in New Udpated Service Layer

--- Management/Library/ServiceLayer.cs
+++ Management/Library/ServiceLayer.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
-using Management.Library;
 using Newtonsoft.Json;
 
 namespace Management.Library
--- Management/Library/ServiceSetting.cs
+++ Management/Library/ServiceSetting.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Net.Http;
-using Management.Library;
 
 namespace Management.Library
 {
